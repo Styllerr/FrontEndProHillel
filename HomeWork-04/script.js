738268e9e010bf52bfaa8bff6b74ba79dfbbe7f1
Styllerr <cork@ukr.net>
2020-02-06 01:40:37 +0200

    final 4 homework

--- HomeWork-04/script.js
+++ HomeWork-04/script.js
@@ -1,11 +1,6 @@
 var userLogin = prompt('Enter your Login:');
-var patternLogin = /^[A-Z][A-Za-z0-9_]*\d{2}$/g;
-/* Проверка
-console.log(userLogin, userLogin.match(patternLogin)); */
+var patternLogin = /^[A-Z][A-Za-z0-9_]*\d{2}$/;
 console.log(patternLogin.test(userLogin));
-
 var userPassword = prompt('Enter your password:');
-var patternPassword = /^[a-z]{1,5}$/g;
-/* Проверка
-console.log(userPassword, userPassword.match(patternPassword)); */
+var patternPassword = /^[a-zа-яё]{1,5}$/;
 console.log(patternPassword.test(userPassword));
