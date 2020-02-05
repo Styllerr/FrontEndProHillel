c3d5f952c3e4ebfcebb5c35123438a4bbcec55d6
Evgeniy Chukavov <cork@ukr.net>
2020-02-05 14:06:03 +0200

    finish 4 homework

--- HomeWork-04/script.js
+++ HomeWork-04/script.js
@@ -1,11 +1,6 @@
 var userLogin = prompt('Enter your Login:');
 var patternLogin = /^[A-Z][A-Za-z0-9_]*\d{2}$/g;
-/* Проверка
-console.log(userLogin, userLogin.match(patternLogin)); */
 console.log(patternLogin.test(userLogin));
-
 var userPassword = prompt('Enter your password:');
-var patternPassword = /^[a-z]{1,5}$/g;
-/* Проверка
-console.log(userPassword, userPassword.match(patternPassword)); */
+var patternPassword = /^[a-z]{1,5}$|^[а-яё]{1,5}$/g;
 console.log(patternPassword.test(userPassword));
