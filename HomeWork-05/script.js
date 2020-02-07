551e3c01c265716e4a6e7b924c2c647db0de9ea7
Styllerr <cork@ukr.net>
2020-02-07 14:51:48 +0200

    finish

--- HomeWork-05/script.js
+++ HomeWork-05/script.js
@@ -1,44 +1,50 @@
 var variant = ['камень', 'ножницы', 'бумага'],
-    resultList = ['победил!!!', 'проиграл :('];
-var compVariant = variant[Math.floor(Math.random()*3)];
-console.log('Компьютер выбрасывает:', compVariant);
-var userVariant = prompt('Вы показываете: Камень, Ножницы или Бумага?');
-if (userVariant === null) {
-    alert('Зря! А могли бы поиграть...');
-} else {
-    userVariant = userVariant.toLocaleLowerCase();
-    var userVariantIndex = variant.indexOf(userVariant);
-    if(userVariantIndex === -1) {
-        alert('Проверте правильность ввода слова');
+    resultList = ['победил!!!', 'проиграл :('],
+    compVarian,
+    userVariant,
+    ready = true;
+while (ready === true) {
+    compVariant = variant[Math.floor(Math.random() * 3)];
+    userVariant = prompt('Вы показываете: Камень, Ножницы или Бумага?');
+
+    if (userVariant === null) {
+        alert('Зря! А могли бы поиграть...');
     } else {
-        if(userVariant === compVariant) {
-            alert('Ничья!\n' + 'Вы оба выбрали' + ' ' + userVariant);
+        userVariant = userVariant.toLowerCase();
+        var userVariantIndex = variant.indexOf(userVariant);
+        if (userVariantIndex === -1) {
+            alert('Проверте правильность ввода слова');
         } else {
-            switch (compVariant) {
-                case 'камень':
-                    if (userVariantIndex === 2) {
-                         result = 0;
-                    } else {
-                        result = 1;
-                    }
-                    break;
-                case 'ножницы':
-                    if (userVariantIndex === 0) {
-                        result = 0;
-                   } else {
-                       result = 1;
-                   }
-                   break;
-                case 'бумага':
-                    if (userVariantIndex === 1) {
-                        result = 0;
-                   } else {
-                       result = 1;
-                   }
-                    break;
+            if (userVariant === compVariant) {
+                alert('Ничья!\n' + 'Вы оба выбрали' + ' ' + userVariant);
+            } else {
+                switch (compVariant) {
+                    case 'камень':
+                        if (userVariantIndex === 2) {
+                            result = 0;
+                        } else {
+                            result = 1;
+                        }
+                        break;
+                    case 'ножницы':
+                        if (userVariantIndex === 0) {
+                            result = 0;
+                        } else {
+                            result = 1;
+                        }
+                        break;
+                    case 'бумага':
+                        if (userVariantIndex === 1) {
+                            result = 0;
+                        } else {
+                            result = 1;
+                        }
+                        break;
+                }
+                alert('Ты' + ' ' + resultList[result] + '.\n' + 'Твой выбор:'
+                    + ' ' + userVariant + ' ' + 'против' + ' ' + compVariant);
             }
-            alert('Ты' + ' ' + resultList[result] + '.\n' + 'Твой выбор:' 
-            + ' ' + userVariant + ' ' + 'против' + ' ' + compVariant);
         }
     }
+    ready = confirm('Поиграем еще?');
 }
