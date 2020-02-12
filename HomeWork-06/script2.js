d696f5e801eaac7da0fdebaeec818bf400a3a961
Styllerr <cork@ukr.net>
2020-02-12 17:10:12 +0200

    finish 2nd task

--- HomeWork-06/script2.js
+++ HomeWork-06/script2.js
@@ -1,20 +1,18 @@
 var defaultString = 'hello MY name is JavaScript and I like your code styLe';
-var stringArray = defaultString.split(' ');
-console.log(stringArray);
-for (var i = 0; i < stringArray.length; i++) {
+var stringArray = defaultString.split(' '),                             //разбираем предложение на слова
+    newStringArray;
+for (var i = 0; i < stringArray.length; i++) {                          //ищем слова с прописной буквой
     if(stringArray[i] != stringArray[i].toLowerCase()) {
-        for (var j = 0; j < stringArray[i].length; j++) {
-            if(stringArray[i][j] === stringArray[i][j].toLowerCase()) {
-                console.log('маленькая:', stringArray[i][j]);
-                stringArray[i][j] = stringArray[i][j].toUpperCase();
-                console.log('теперь большая', stringArray[i][j]);
+        newStringArray = stringArray[i].split('');                      //разбираем это слово на буквы
+        for (var j = 0; j < newStringArray.length; j++) {
+            if(newStringArray[j] === newStringArray[j].toLowerCase()) { //делаем замену букв
+                newStringArray[j] = newStringArray[j].toUpperCase();
             } else {
-                stringArray[i][j] = stringArray[i][j].toLowerCase();
-                console.log(stringArray[i][j]);
+                newStringArray[j] = newStringArray[j].toLowerCase();
             }
-            
+            stringArray[i] = newStringArray.join('');                   //собираем из новых букв слово
         }
-        console.log(stringArray[i]);
     }
-    
 }
+result = stringArray.join(" ");                                         //собираем предложение с измененными словами
+alert(result + '- стало\n' + defaultString + '- а так было');
