ca19e9fb25dfd7905ab2d4e259b158a2cca629e4
Styllerr <cork@ukr.net>
2020-02-16 05:36:32 +0200

    finish hw7

--- HomeWork-07/script.js
+++ HomeWork-07/script.js
@@ -1,36 +1,26 @@
 var firstArray = [5, 97, 33, 'brother', 54, null, 'cat', 45, 'mouse', 98, '', 'homework', 45, 45, 75],
     secondArray = ['cat', 5, 'somebody', 'brother', 33, 4, 98, 'home', 5, 45, '', 93, 54, 'brother', null],
     resultArray = [],
-    repidFlag = true; // флаг проверки уникальности в массиве результатов
+    repidFlag = true;                        // флаг проверки уникальности в массиве результатов
 for (i = 0; i < firstArray.length; i++) {
-    let notOneFlag = 0; // флаг проверки уникальности во втором массиве
-    repidFlag = resultArray.every(function (item) {
+    let notOneFlag = 0;                      // флаг проверки уникальности во втором массиве
+    repidFlag = resultArray.every(function(item) {
         return item != firstArray[i];
     })
-    if (repidFlag === true) {
-        secondArray.forEach(function (item) {
+    if (repidFlag === true) {               //если этого элемента еще нет в новом массиве
+        secondArray.forEach(function(item) {
             if (firstArray[i] === item && notOneFlag === 0) {
-                notOneFlag = 1;
                 resultArray.push(item);
+                notOneFlag = 1;              //флаг, что этот совпавший элемент уже добавлен в новый массив
             }
         });
     }
 }
-console.log(resultArray);
-
-resultArray.forEach(function(item) {
-    console.log(typeof item);
-    switch (typeof(item)) {
-        case 'number':
-            if (item % 5 === 0) {
-                item = 'FIVE';
-            }
-            break;
-        case 'string':
-            if (item.length > 5) {
-                item = 'FSTR';
-            }
-            break;
+resultArray.forEach(function(item, index) { //замена чилел кратных 5 и стрнгов более 5 символов
+    if (typeof item === "number" && item % 5 === 0) {
+        resultArray[index] = 'FIVE';
+    } else if (typeof item === "string" && item.length > 5) {
+        resultArray[index] = 'FSTR';
     }
 });
 console.log(resultArray);
