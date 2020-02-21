2ed6713d5dc207ec5071d448c5b129a9f66df0da
Evgeniy Chukavov <cork@ukr.net>
2020-02-21 21:03:10 +0200

    final hw9

--- HomeWork-09/script-1.js
+++ HomeWork-09/script-1.js
@@ -1,16 +1,31 @@
-let firstObject = {
+let oldPrice = {
     apple: 29,
+    tomato: 30,
     peach: 78,
     pear: 35,
     raspberries: 100,
     potatoes: 14,
-    carrot: 5
+    carrot: 5,
 }
-let secondObject = {
-    peach: 29,
+let newPrice = {
+    peach: 63,
     strawberry: 90,
     apple: 25,
     pear: 32,
     raspberries: 100,
-    carrot: 5
+    carrot: 5,
+    tomato: 30,
 };
+let result = {};
+function market() {
+    for(let productOld in oldPrice) {
+        for(let productNew in newPrice) {
+            if(productOld === productNew 
+                && oldPrice[productOld] === newPrice[productNew]) {
+                    result[newPrice[productNew]] = productNew;
+                }
+        }
+    }
+    return result;
+}
+console.log(market());
