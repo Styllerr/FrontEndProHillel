341813b6635ecffdee10c2406afdcc44193a9fad
Styllerr <cork@ukr.net>
2020-04-08 11:01:41 +0300

    hw17 50%

--- HomeWork-17/script.js
+++ HomeWork-17/script.js
@@ -1,49 +1,85 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let listContact = [];
     //Представление 1
     class PageStart {
         constructor() {
-            this.contactFirstName = '<label>Имя:<input type="text" class="contact_firstName"></label>',
-            this.contactSecondName = '<label>Фамилия:<input type="text" class="contact_secondName"></label>',
-            this.contactAge = '<label>Возраст:<input type="number" class="contact_age"></label>',
-            this.contactAddButton = '<button class="contact_addButton">Добавить</button>',
-            this.showButton = '<button id="showButton">Список</button>';
+            this.contactFirstName = '<label>Имя:<input type="text" class="contact_firstName" autofocus></label>',
+                this.contactSecondName = '<label>Фамилия:<input type="text" class="contact_secondName"></label>',
+                this.contactAge = '<label>Возраст:<input type="number" class="contact_age"></label>',
+                this.contactAddButton = '<button class="contact_addButton">Добавить</button>',
+                this.showButton = '<button id="showButton">Список</button>';
         }
         createPage() {
             document.body.innerHTML = '<div id="wraper_contact"></div>' + this.showButton;
-            wraper_contact.innerHTML = `${this.contactFirstName}${this.contactSecondName}${this.contactAge}${this.contactAddButton}`;
+            let wraperContact = document.getElementById('wraper_contact');
+            wraperContact.innerHTML = `${this.contactFirstName}${this.contactSecondName}${this.contactAge}${this.contactAddButton}`;
+        }
+    }
+
+    //Представление 2
+    class PageList {
+        constructor() {
+            this.contactAddButton = '<button class="return_add">Вернуться на страницу Добавить</button>';
+        }
+        createPage() {
+            document.body.innerHTML = '<div id="wraper_list"><ol></ol></div>' + this.contactAddButton;
+            let placeList = document.querySelector('#wraper_list>ol');
+            let indexContact = listContact.length;
+
+            for (let i = 0; i < indexContact; i++) {
+                let listItem = document.createElement('li');
+                let cross = document.createElement('span');
+                cross.className = 'mark';
+                listItem.innerText = listContact[i].secondName;
+
+                placeList.append(listItem);
+                listItem.append(cross);
+            }
         }
     }
 
 
 
-    
-    
     //МОДЕЛЬ
     class Contact {
         constructor(firstName, secondName, age) {
-            this.name = firstName;
+            this.firstName = firstName;
             this.secondName = secondName;
             this.age = age;
         }
-    }
-    //Контроллер
-    class Event {
-        constructor(view) {
-            this.view = view;
+        addInList(newContact){
+            
+            listContact.push(newContact);
+            firstName.value = '';
+            secondName.value = '';
+            age.value = '';
+            firstName.focus();
+            console.log(listContact);
         }
+    }
+
+    //Контроллер
+
+    class Controller {
         static addContact() {
-            // firstName = firstName.value;
-            newContact = new Contact();
+            let newContact = new Contact(firstName.value, secondName.value, age.value);
+            newContact.addInList(newContact);
+        }
+        static showList() {
+            let pageList = new PageList();
+            pageList.createPage();
         }
     }
     let pageOne = new PageStart();
     pageOne.createPage();
-    let control = new Event(pageOne);
-
-    const firstName = document.querySelector('input.contact_firstName'),
+    
+    let firstName = document.querySelector('input.contact_firstName'),
         secondName = document.querySelector('input.contact_secondName'),
         age = document.querySelector('input.contact_age');
-    
-    const addButton = document.querySelector('button.contact_addButton');
-    addButton.addEventListener('click', Event.addContackt(firstName.value, secondName.value, age.value))
+
+    const addButton = document.querySelector('button.contact_addButton'),
+        showButton = document.getElementById('showButton');
+
+    addButton.addEventListener('click', Controller.addContact);
+    showButton.addEventListener('click', Controller.showList);
 })
