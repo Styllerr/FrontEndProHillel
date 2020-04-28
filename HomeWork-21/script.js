2c9dc3f720900090ef7bc094bbcc2191b60ea682
Evgeniy Chukavov <cork@ukr.net>
2020-04-28 15:15:21 +0300

    hw21

--- HomeWork-21/script.js
+++ HomeWork-21/script.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+let xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', true);
+xhttp.send()
 })
