70791ef4ae15a0ba9076b923853245786809326c
Styllerr <cork@ukr.net>
2020-05-05 11:59:01 +0300

    hw24 process

--- HomeWork-24/index.js
+++ HomeWork-24/index.js
@@ -1,9 +1,26 @@
-const http = require('http');
+/* const http = require('http');
 const path = require('path');
 const fs = require('fs');
-
-http.createServer((req, res) => {
-    res.end = 'Hello';
-}).listen(3000, () => {
-    console.log('Srver has starting...')
+ */
+/* const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile(path.join(__dirname, 'pub', 'index.html'), (err, data) => {
+        if (err) {
+            throw new Error(err);
+        }
+        let page = data.toString();
+        res.end(page);
+    })
 })
+server.listen(3000, () => {
+    console.log('Server has starting...');
+}) */
+
+let express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => res.send('Hello Express!!!'));
+app.get('/stud', (req, res) => res.send('Нет студентов'));
+
+app.listen(3000, () => console.log('Server running...'));
